a45ac0589c0a480e69a4bf56a9f812757b6e7f68
Daniel Ally <d.ally@cern.ch>
2024-03-11 16:08:19 +0100

    Checked for stronger photon-electron association, and if there was an associating pixel seed

--- PhElRecoEff.py
+++ PhElRecoEff.py
@@ -27,18 +27,26 @@ gen_electrons = gen_electrons[(gen_electrons.pt > GenPtMin) & (abs(gen_electrons
 RecoElectronsFromGen = electrons[(abs(electrons.matched_gen.distinctParent.distinctParent.pdgId) == 1000015)]
 RecoElectronsFromGen = RecoElectronsFromGen[(RecoElectronsFromGen.pt > GenPtMin) & (abs(RecoElectronsFromGen.eta) < GenEtaMax)]
 
+photons = photons[photons.pixelSeed == True]
+RecoPhotons = photons[(abs(gpart[photons.genPartIdx.compute()].pdgId) == 11)
+                  & (abs(gpart[photons.genPartIdx.compute()].distinctParent.distinctParent.pdgId) == 1000015)
+                  & (gpart[photons.genPartIdx.compute()].pt > GenPtMin)
+                  & (abs(gpart[photons.genPartIdx.compute()].eta) < GenEtaMax)]
 RecoPhotonsFromGen   = gpart[photons.genPartIdx.compute()]
 RecoPhotonsFromGen   = RecoPhotonsFromGen[abs(RecoPhotonsFromGen.pdgId) == 11]
 RecoPhotonsFromGen   = RecoPhotonsFromGen[abs(RecoPhotonsFromGen.distinctParent.distinctParent.pdgId) == 1000015]
 RecoPhotonsFromGen   = RecoPhotonsFromGen[(RecoPhotonsFromGen.pt > GenPtMin) & (abs(RecoPhotonsFromGen.eta) < GenEtaMax)]
+dpt_pt = abs(ak.flatten(RecoPhotonsFromGen.pt, axis = None) - ak.flatten(RecoPhotons.pt, axis = None))/ak.flatten(RecoPhotonsFromGen.pt, axis = None)
+dpt_pt_mask = dpt_pt < 0.3
+RecoPhotonsFromGen = RecoPhotonsFromGen[dpt_pt_mask]
 
 RecoLowPtElecFromGen = gpart[lowptelectrons.genPartIdx.compute()]
 RecoLowPtElecFromGen = RecoLowPtElecFromGen[abs(RecoLowPtElecFromGen.distinctParent.distinctParent.pdgId) == 1000015]
 RecoLowPtElecFromGen = RecoLowPtElecFromGen[(RecoLowPtElecFromGen.pt > GenPtMin) & (abs(RecoLowPtElecFromGen.eta) < GenEtaMax)]
 
-print("Number of gen electrons:", len(ak.flatten(gen_electrons.pt.compute(), axis = None)))
-print("Number of reco electrons from Electron collection:", len(ak.flatten(RecoElectronsFromGen.pt.compute(), axis = None)))
-print("Number of reco electrons from LowPtElectron collection:", len(ak.flatten(RecoLowPtElecFromGen.pt.compute(), axis = None)))
-print("Number of reco electrons from Photon collection:", len(ak.flatten(RecoPhotonsFromGen.pt.compute(), axis = None)))
+#print("Number of gen electrons:", len(ak.flatten(gen_electrons.pt.compute(), axis = None)))
+#print("Number of reco electrons from Electron collection:", len(ak.flatten(RecoElectronsFromGen.pt.compute(), axis = None)))
+#print("Number of reco electrons from LowPtElectron collection:", len(ak.flatten(RecoLowPtElecFromGen.pt.compute(), axis = None)))
+#print("Number of reco electrons from Photon collection:", len(ak.flatten(RecoPhotonsFromGen.pt.compute(), axis = None)))
 
-#makeEffPlot("e", "gammacomp", ["Electrons", "LowPtElectrons", "Photons"], "pt", 16, 20, 100, 5, "[GeV]", [gen_electrons.pt.compute(),] * 3, [RecoElectronsFromGen.pt.compute(), RecoLowPtElecFromGen.pt.compute(), RecoPhotonsFromGen.pt.compute()], 0, file)
+makeEffPlot("e", "gammacomp_pixelseed_energyassoc", ["Electrons", "LowPtElectrons", "Photons"], "pt", 16, 20, 100, 5, "[GeV]", [gen_electrons.pt.compute(),] * 3, [RecoElectronsFromGen.pt.compute(), RecoLowPtElecFromGen.pt.compute(), RecoPhotonsFromGen.pt.compute()], 0, file)
--- Run3NanoAOD_Dict.py
+++
@@ -1,8 +0,0 @@
-Run3_Dict = {
-  #"Run3_TT_13p6TeV" : "TT_TuneCP5_13p6TeV_powheg-pythia8_with-disTauTagScore.root",
-  "Run3_Stau_500GeV_100mm": "Stau_M_500_100mm_Summer22EE_NanoAOD.root", 
-  "Run3_Stau_500GeV_10mm": "Stau_M_500_10mm_Summer22EE_NanoAOD.root", 
-  "Run3_Stau_100GeV_10mm": "Stau_M_100_10mm_Summer22EE_NanoAOD.root", 
-  "Run3_Stau_100GeV_100mm" : "Stau_M_100_100mm_Summer22EE_NanoAOD.root"
-  }
-            
