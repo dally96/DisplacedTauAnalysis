b632eaccc3550f71dda40cf8fdb497c011944f43
dally96 <dally@vols.utk.edu>
2024-03-20 11:12:31 +0100

    Photon analysis

--- PhElRecoEff.py
+++ PhElRecoEff.py
@@ -11,7 +11,7 @@ from leptonPlot import *
 file = "Staus_M_100_100mm_13p6TeV_Run3Summer22_lpcdisptau_NanoAOD_ExtraDisMuonBranches.root"
 
 events = NanoEventsFactory.from_root({file:"Events"}).events()
-
+ROOT.gStyle.SetOptStat(0)
 
 gpart = events.GenPart
 gvistau = events.GenVisTau
@@ -31,38 +31,132 @@ RecoElectronsFromGen = RecoElectronsFromGen[(RecoElectronsFromGen.pt > GenPtMin)
 #photons = photons[photons.pixelSeed == True]
 photons = photons[(photons.electronIdx != -1) & (photons.electronIdx < ak.num(electrons))]
 phelectrons = electrons[photons.electronIdx.compute()]
-phelectrons = phelectrons[(phelectrons.photonIdx != -1) & (phelectrons.photonIdx < ak.num(photons))]
+#phelectrons = phelectrons[(phelectrons.photonIdx != -1) & (phelectrons.photonIdx < ak.num(photons))]
 nonphelectrons = phelectrons[(phelectrons.photonIdx == -1) | (phelectrons.photonIdx >= ak.num(photons))]
 phelphotons = photons[phelectrons.photonIdx.compute()]
 nonphelphotons = photons[(electrons[photons.electronIdx.compute()].photonIdx == -1) | (electrons[photons.electronIdx.compute()].photonIdx>= ak.num(photons))]
 
-photons_pt = ak.flatten(photons.pt.compute(), axis = None)
-phelphotons_pt = ak.flatten(phelphotons.pt.compute(), axis = None)
-nonphelphotons_pt = ak.flatten(nonphelphotons.pt.compute(), axis = None)
+#photons_pt = ak.flatten(photons.pt.compute(), axis = None)
+#phelphotons_pt = ak.flatten(phelphotons.pt.compute(), axis = None)
+#nonphelphotons_pt = ak.flatten(nonphelphotons.pt.compute(), axis = None)
+#
+#photons_eta = ak.flatten(photons.eta.compute(), axis = None)
+#phelphotons_eta = ak.flatten(phelphotons.eta.compute(), axis = None)
+#nonphelphotons_eta = ak.flatten(nonphelphotons.eta.compute(), axis = None)
+#
+#photons_hoe = ak.flatten(photons.hoe.compute(), axis = None)
+#phelphotons_hoe = ak.flatten(phelphotons.hoe.compute(), axis = None)
+#nonphelphotons_hoe = ak.flatten(nonphelphotons.hoe.compute(), axis = None)
+#
+#for pt in phelphotons_pt:
+#  if pt not in photons_pt:
+#    nonphelphotons_pt.append(pt)
+#
+#for eta in phelphotons_eta:
+#  if eta not in photons_eta:
+#    nonphelphotons_eta.append(eta)
+#
+#for hoe in phelphotons_hoe:
+#  if hoe not in photons_hoe:
+#    nonphelphotons_hoe.append(hoe)
 
-photons_eta = ak.flatten(photons.eta.compute(), axis = None)
-phelphotons_eta = ak.flatten(phelphotons.eta.compute(), axis = None)
-nonphelphotons_eta = ak.flatten(nonphelphotons.eta.compute(), axis = None)
+phelectrons = phelectrons[(abs(phelectrons.matched_gen.distinctParent.distinctParent.pdgId) == 1000015)]
+phelectrons = phelectrons[(phelectrons.pt > GenPtMin) & (abs(phelectrons.eta) < GenEtaMax)]
 
-photons_hoe = ak.flatten(photons.hoe.compute(), axis = None)
-phelphotons_hoe = ak.flatten(phelphotons.hoe.compute(), axis = None)
-nonphelphotons_hoe = ak.flatten(nonphelphotons.hoe.compute(), axis = None)
+RecoElectronsFromGenList = ak.drop_none(RecoElectronsFromGen.pt).compute().tolist()
+phelectronsList = ak.drop_none(phelectrons.pt).compute().tolist()
 
-for pt in phelphotons_pt:
-  if pt not in photons_pt:
-    nonphelphotons_pt.append(pt)
+photoelectrons = electrons[photons.electronIdx.compute()]
+photoelectrons = photoelectrons[(photoelectrons.pt > GenPtMin) & (abs(photoelectrons.eta) < GenEtaMax)]
 
-for eta in phelphotons_eta:
-  if eta not in photons_eta:
-    nonphelphotons_eta.append(eta)
+electrons = electrons[(electrons.pt > GenPtMin) & (abs(electrons.eta) < GenEtaMax)]
 
-for hoe in phelphotons_hoe:
-  if hoe not in photons_hoe:
-    nonphelphotons_hoe.append(hoe)
+electronList_pt = ak.drop_none(electrons.pt).compute().tolist()
+photoelectronList_pt = ak.drop_none(photoelectrons.pt).compute().tolist()
+
+electronList_eta = ak.drop_none(electrons.eta).compute().tolist()
+photoelectronList_eta = ak.drop_none(photoelectrons.eta).compute().tolist()
+
+electronList_hoe = ak.drop_none(electrons.hoe).compute().tolist()
+photoelectronList_hoe = ak.drop_none(photoelectrons.hoe).compute().tolist()
+
+electronList_dxy = abs(ak.drop_none(electrons.dxy)).compute().tolist()
+photoelectronList_dxy = abs(ak.drop_none(photoelectrons.dxy)).compute().tolist()
+
+diff_pt = [np.setdiff1d(subarr1, subarr2) for subarr1, subarr2 in zip(electronList_pt, photoelectronList_pt)]
+diff_eta = [np.setdiff1d(subarr1, subarr2) for subarr1, subarr2 in zip(electronList_eta, photoelectronList_eta)]
+diff_hoe = [np.setdiff1d(subarr1, subarr2) for subarr1, subarr2 in zip(electronList_hoe, photoelectronList_hoe)]
+diff_dxy = [np.setdiff1d(subarr1, subarr2) for subarr1, subarr2 in zip(electronList_dxy, photoelectronList_dxy)]
 
 
+l_ele_pt = ROOT.TLegend()
+h_ele_pt = ROOT.TH1F('h_ele_pt', ';pt [GeV]; Number of electrons', 40, 20, 100)
+h_diff_pt = ROOT.TH1F('h_diff_pt', ';pt [GeV]; Number of electrons', 40, 20, 100)
+for i in ak.flatten(photoelectronList_pt, axis = None):
+  h_ele_pt.Fill(i)
+for i in ak.flatten(diff_pt, axis = None):
+  h_diff_pt.Fill(i)
+h_ele_pt.Scale(1/len(ak.flatten(photoelectronList_pt, axis = None)))
+h_diff_pt.Scale(1/len(ak.flatten(diff_pt, axis = None)))
+h_diff_pt.SetLineColor(3)
+l_ele_pt.AddEntry(h_ele_pt, "matched electrons")
+l_ele_pt.AddEntry(h_diff_pt, "electrons not part of photon collection")
+h_diff_pt.Draw("HISTE")
+h_ele_pt.Draw("SAMEHISTE")
+l_ele_pt.Draw()
+can.SaveAs("h_ele_pt.pdf")
 
-#print("Photon selection", photons[(electrons[photons.electronIdx.compute()].photonIdx == -1) | (electrons[photons.electronIdx.compute()].photonIdx > ak.num(photons))].compute())
+l_ele_eta = ROOT.TLegend()
+h_ele_eta = ROOT.TH1F('h_ele_eta', ';#eta; Number of electrons', 48, -2.4, 2.4)
+h_diff_eta = ROOT.TH1F('h_diff_eta', ';#eta; Number of electrons', 48, -2.4, 2.4)
+for i in ak.flatten(photoelectronList_eta, axis = None):
+  h_ele_eta.Fill(i)
+for i in ak.flatten(diff_eta, axis = None):
+  h_diff_eta.Fill(i)
+h_ele_eta.Scale(1/len(ak.flatten(photoelectronList_eta, axis = None)))
+h_diff_eta.Scale(1/len(ak.flatten(diff_eta, axis = None)))
+h_diff_eta.SetLineColor(3)
+l_ele_eta.AddEntry(h_ele_eta, "matched electrons")
+l_ele_eta.AddEntry(h_diff_eta, "electrons not part of photon collection")
+h_diff_eta.Draw("HISTE")
+h_ele_eta.Draw("SAMEHISTE")
+l_ele_eta.Draw()
+can.SaveAs("h_ele_eta.pdf")
+
+l_ele_hoe = ROOT.TLegend()
+h_ele_hoe = ROOT.TH1F('h_ele_hoe', ';H/E; Number of electrons', 25, 0, 1)
+h_diff_hoe = ROOT.TH1F('h_diff_hoe', ';H/E; Number of electrons', 25, 0, 1)
+for i in ak.flatten(photoelectronList_hoe, axis = None):
+  h_ele_hoe.Fill(i)
+for i in ak.flatten(diff_hoe, axis = None):
+  h_diff_hoe.Fill(i)
+h_ele_hoe.Scale(1/len(ak.flatten(photoelectronList_hoe, axis = None)))
+h_diff_hoe.Scale(1/len(ak.flatten(diff_hoe, axis = None)))
+h_diff_hoe.SetLineColor(3)
+l_ele_hoe.AddEntry(h_ele_hoe, "matched electrons")
+l_ele_hoe.AddEntry(h_diff_hoe, "electrons not part of photon collection")
+h_ele_hoe.Draw("HISTE")
+h_diff_hoe.Draw("SAMEHISTE")
+l_ele_hoe.Draw()
+can.SaveAs("h_ele_hoe.pdf")
+
+l_ele_dxy = ROOT.TLegend()
+h_ele_dxy = ROOT.TH1F('h_ele_dxy', ';d_{xy}; Number of electrons', 30, 0, 15)
+h_diff_dxy = ROOT.TH1F('h_diff_dxy', ';d_{xy}; Number of electrons', 30, 0, 15)
+for i in ak.flatten(photoelectronList_dxy, axis = None):
+  h_ele_dxy.Fill(i)
+for i in ak.flatten(diff_dxy, axis = None):
+  h_diff_dxy.Fill(i)
+h_ele_dxy.Scale(1/len(ak.flatten(photoelectronList_dxy, axis = None)))
+h_diff_dxy.Scale(1/len(ak.flatten(diff_dxy, axis = None)))
+h_diff_dxy.SetLineColor(3)
+l_ele_dxy.AddEntry(h_ele_dxy, "matched electrons")
+l_ele_dxy.AddEntry(h_diff_dxy, "electrons not part of photon collection")
+h_ele_dxy.Draw("HISTE")
+h_diff_dxy.Draw("SAMEHISTE")
+l_ele_dxy.Draw()
+can.SaveAs("h_ele_dxy.pdf")
+
 RecoPhotonsFromGen   = gpart[photons.genPartIdx.compute()]
 RecoPhotonsFromGen   = RecoPhotonsFromGen[abs(RecoPhotonsFromGen.pdgId) == 11]
 RecoPhotonsFromGen   = RecoPhotonsFromGen[abs(RecoPhotonsFromGen.distinctParent.distinctParent.pdgId) == 1000015]
@@ -75,10 +169,10 @@ RecoLowPtElecFromGen = RecoLowPtElecFromGen[(RecoLowPtElecFromGen.pt > GenPtMin)
 print("Number of gen electrons:", len(ak.flatten(gen_electrons.pt.compute(), axis = None)))
 print("Number of reco electrons from Electron collection:", len(ak.flatten(RecoElectronsFromGen.pt.compute(), axis = None)))
 print("Number of reco electrons from LowPtElectron collection:", len(ak.flatten(RecoLowPtElecFromGen.pt.compute(), axis = None)))
-print("Number of reco electrons from Photon collection:", len(ak.flatten(RecoPhotonsFromGen.pt.compute(), axis = None)))
+print("Number of reco electrons from Photon collection:", len(ak.flatten(phelectrons.pt.compute(), axis = None)))
 
-#makeEffPlot("e", "gammacomp_enotgam", ["Electrons", "LowPtElectrons", "Photons"], "pt", 16, 20, 100, 5, "[GeV]", [gen_electrons.pt.compute(),] * 3, [RecoElectronsFromGen.pt.compute(), RecoLowPtElecFromGen.pt.compute(), RecoPhotonsFromGen.pt.compute()], 0, file)
+makeEffPlot("e", "gammacomp_gammaMatchedToGenE", ["Electrons", "Photons which are matched to a dis gen electron"], "pt", 16, 20, 100, 5, "[GeV]", [gen_electrons.pt.compute(),] * 2, [RecoElectronsFromGen.pt.compute(), RecoPhotonsFromGen.pt.compute()], 0, file)
 #makeEffPlot("e", "gammacomp_matchedgam", ["Electrons", "LowPtElectrons", "Photons"], "hoe", 0, 1, 20, 0.05, "", [gen_electrons.hoe.compute(),] * 3, [RecoElectronsFromGen.eta.compute(), RecoLowPtElecFromGen.pt.compute(), RecoPhotonsFromGen.pt.compute()], 0, file)
-makeEffPlot("photons", "e_not_photon", ["#gamma associated to e associated to same #gamma",  "#gamma that weren't associated to e"] , "pt", 20, 0, 100, 5, "[GeV]", [photons_pt,] * 2, [phelphotons_pt, nonphelphotons_pt], 0, file)
-makeEffPlot("photons", "e_not_photon", ["#gamma associated to e associated to same #gamma",  "#gamma that weren't associated to e"] , "#eta", 24, -2.4, 2.4, 0.2, "", [photons_eta,] * 2, [phelphotons_eta, nonphelphotons_eta], 0, file) 
-makeEffPlot("photons", "e_not_photon", ["#gamma associated to e associated to same #gamma",  "#gamma that weren't associated to e"] , "hoe", 20, 0, 1, 0.05, "", [photons_hoe,] * 2, [phelphotons_hoe, nonphelphotons_hoe], 0, file) 
+#makeEffPlot("photons", "e_not_photon", ["#gamma associated to e associated to same #gamma",  "#gamma that weren't associated to e"] , "pt", 20, 0, 100, 5, "[GeV]", [photons_pt,] * 2, [phelphotons_pt, nonphelphotons_pt], 0, file)
+#makeEffPlot("photons", "e_not_photon", ["#gamma associated to e associated to same #gamma",  "#gamma that weren't associated to e"] , "#eta", 24, -2.4, 2.4, 0.2, "", [photons_eta,] * 2, [phelphotons_eta, nonphelphotons_eta], 0, file) 
+#makeEffPlot("photons", "e_not_photon", ["#gamma associated to e associated to same #gamma",  "#gamma that weren't associated to e"] , "hoe", 20, 0, 1, 0.05, "", [photons_hoe,] * 2, [phelphotons_hoe, nonphelphotons_hoe], 0, file) 
