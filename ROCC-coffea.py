c3dae0e1a56e9d2ef4394034f319529f87d32808
j-s-ashley <jashley6@vols.utk.edu>
2024-11-03 21:04:03 -0500

    Create starter coffea script

---
+++ ROCC-coffea.py
@@ -0,0 +1,14 @@
+import awkward as ak
+from coffea.nanoevents import NanoEventsFactory, NanoAODDSchema
+
+NanoAODSchema.warn_missing_crossrefs = False
+
+fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root"
+
+events = NanoEventsFactory.from_root(
+  {fname: "Events"},
+  schemaclass = NanoAODSchema,
+  metadata = {"dataset": "signal"},
+).events
+
+events.Generator.id1.compute()
