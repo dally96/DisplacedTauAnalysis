59e839483355002d25558d0bf47874df8a1f7629
j-s-ashley <jashley6@vols.utk.edu>
2024-11-07 16:09:48 +0100

    :bug: Try coffea-approved example dataset

--- ROCC-coffea.py
+++ ROCC-coffea.py
@@ -3,7 +3,8 @@ from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
 NanoAODSchema.warn_missing_crossrefs = False
 
-fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root"
+fname = "https://raw.githubusercontent.com/CoffeaTeam/coffea/master/tests/samples/nano_dy.root"
+#fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root"
 
 events = NanoEventsFactory.from_root(
   {fname: "Events"},
@@ -11,4 +12,4 @@ events = NanoEventsFactory.from_root(
   metadata={"dataset": "signal"},
 ).events
 
-events.Generator.fields
+events.GenJet.fields
