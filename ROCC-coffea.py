af427244aeb40ea51d14700269185ae39b97a6a0
j-s-ashley <jashley6@vols.utk.edu>
2024-11-07 16:27:36 +0100

    :bug::clown_face: I understand spacing just fine, OneNote does not

--- ROCC-coffea.py
+++ ROCC-coffea.py
@@ -3,11 +3,12 @@ from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
 NanoAODSchema.warn_missing_crossrefs = False
 
-fname = "https://raw.githubusercontent.com/CoffeaTeam/coffea/master/tests/samples/nano_dy.root"
+fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root"
+
 events = NanoEventsFactory.from_root(
     {fname: "Events"},
     schemaclass=NanoAODSchema,
-    metadata={"dataset": "DYJets"},
+    metadata={"dataset": "signal"},
 ).events()
 
 print(events.Generator.fields)
