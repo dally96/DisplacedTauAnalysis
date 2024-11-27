0c50f3f5df35226a6245718513fdb7709ed954f1
j-s-ashley <jashley6@vols.utk.edu>
2024-11-27 15:39:31 +0100

    Implement Brandi's selection cut method

--- ROCC-coffea.py
+++ ROCC-coffea.py
@@ -6,6 +6,10 @@ import awkward as ak
 from coffea.nanoevents import NanoEventsFactory, NanoAODSchema, PFNanoAODSchema
 from coffea.analysis_tools import PackedSelection
 
+# --- Selection Cut Parameters --- #
+min_pT = 20
+max_eta = 2.4
+
 NanoAODSchema.warn_missing_crossrefs = False
 
 fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root"
@@ -16,13 +20,22 @@ events = NanoEventsFactory.from_root(
     metadata={"dataset": "signal"},
     delayed = False).events()
 
-selection = PackedSelection()
-
-selection.add("electron", ak.num(events.Electron))
-selection.add("muon", ak.num(events.Muon))
-selection.add("tau", ak.num(events.Tau))
-
-selection.require(("electron" | "tau" | "muon") = true)
-
-print( len(selection), "leptons out of " )
-print( len(events), "total things" )
+gpart = events.GenPart
+electron_selection = gpart[ (abs(gpart.pdgId) == 11) &\
+        (gpart.pt > min_pT) &\
+        (gpart.eta < max_eta)&\
+        (gpart.hasFlags("isLastCopy")) ]
+muon_selection = gpart[ (abs(gpart.pdgId) == 13) &\
+        (gpart.pt > min_pT) &\
+        (gpart.eta < max_eta)&\
+        (gpart.hasFlags("isLastCopy")) ]
+tau_selection = gpart[ (abs(gpart.pdgId) == 15) &\
+        (gpart.pt > min_pT) &\
+        (gpart.eta < max_eta)&\
+        (gpart.hasFlags("isLastCopy")) ]
+print("ELECTRONS")
+print(electron_selection)
+print("MUONS")
+print(muon_selection)
+print("TAUS")
+print(tau_selection)
