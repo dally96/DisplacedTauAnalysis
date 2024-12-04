244fdf5cc12c77a0c7044641ca8a149a87bde943
j-s-ashley <jashley6@vols.utk.edu>
2024-12-04 02:45:41 +0100

    Processor-based testing

--- coffea_test.py
+++ coffea_test.py
@@ -1,45 +1,54 @@
 import uproot
 import scipy
-import numpy as np
-import matplotlib as mpl
-from matplotlib import pyplot as plt
 import awkward as ak
-import math
-import ROOT
-import array
-import pandas as pd
-from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
-from leptonPlot import *
+import numpy as np
+from coffea import processor
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema, PFNanoAODSchema
 
 NanoAODSchema.warn_missing_crossrefs = False
 
-file = "Staus_M_100_100mm_13p6TeV_Run3Summer22_lpcdisptau_NanoAOD_ExtraDisMuonBranches.root"
+class LeptonProcessor:
+    def __init__(self, eta_cut, pt_cut):
+        self.eta_cut = eta_cut
+        self.pt_cut = pt_cut
 
-#events = NanoEventsFactory.from_root({file:"Events"}, schemaclass=NanoAODSchema).events()[0:21]
-events = NanoEventsFactory.from_root({file:"Events"}).events()
+    def process(self, events):
+        # Select reconstructed leptons (Muons)
+        leptons = events.Muon  # or events.Electron
+        
+        # Access GenPart for matching
+        gen_parts = events.GenPart
+        
+        # Match leptons to GenPart
+        gen_match_mask = (gen_parts.statusFlags & (1 << 13) != 0) & (gen_parts.statusFlags & (1 << 0) != 0)
+        matched_gen_parts = gen_parts[gen_match_mask]
+        
+        # Match reconstructed leptons with generator particles
+        gen_dr = leptons.nearest(matched_gen_parts, axis=1, return_metric=True)
+        matched_leptons = gen_dr[1] < 0.1  # deltaR threshold
 
-gpart = events.GenPart
-electrons = events.Electron
-muons = events.Muon
-staus = gpart[(abs(gpart.pdgId) == 1000015) & (gpart.hasFlags("isLastCopy"))]
+        leptons = leptons[matched_leptons]
 
-#taus = gpart[(abs(gpart.pdgId) == 15) & (abs(gpart.parent.pdgId) == 1000015) & (gpart.hasFlags("isLastCopy"))]
-staus_taus = staus.distinctChildren[(abs(staus.distinctChildren.pdgId) == 15) & (staus.distinctChildren.hasFlags("isLastCopy"))]
-staus_taus = ak.flatten(staus_taus, axis=2)
+        # Apply eta and pT cuts
+        leptons = leptons[(abs(leptons.eta) < self.eta_cut) & (leptons.pt > self.pt_cut)]
 
-gen_mus = staus_taus.distinctChildren[(abs(staus_taus.distinctChildren.pdgId) == 13)]
-gen_electrons = staus_taus.distinctChildren[(abs(staus_taus.distinctChildren.pdgId) == 11)]
-RecoElectronsFromGen = electrons[(abs(electrons.matched_gen.distinctParent.distinctParent.pdgId) == 1000015)]
+        # Count leptons per event
+        n_leptons = np.array([len(leptons[event]) for event in range(len(leptons))])
 
-gen_electrons = gen_electrons[(gen_electrons.pt > 20) & (abs(gen_electrons.eta) < 2.4)]
-RecoElectronsFromGen = RecoElectronsFromGen.matched_gen[(RecoElectronsFromGen.matched_gen.pt > 20) & (abs(RecoElectronsFromGen.matched_gen.eta) < 2.4)]
+        return {
+            "n_leptons": n_leptons,
+        }
 
+processor_instance = LeptonProcessor(eta_cut=2.4, pt_cut=20)
 
-makeEffPlot("e", "coffea_100GeV_100mm", [""], "pt", 16, 20, 100, 5, "[GeV]", [gen_electrons.pt.compute()], [RecoElectronsFromGen.pt.compute()], 0, file) 
+fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root"
 
-#for ev in range(len(events)):
-  #print("The pdgId of the mus in event", ev, "is", gen_mus.pt.compute()[ev])
-#  print("The pt of the electrons in event", ev, "is", gen_electrons.pt.compute()[ev])
-  #print("The gen idx of the matched muon for event", ev, "is", electrons.matched_gen.compute()[ev]) 
-  #print("The dr between reco ele and gen ele for event", ev, "is", dr.compute()[ev])
-  #print("The reco electrons that trace back to gen electrons whcih decay from staus for event", ev, "are", RecoElectronsFromGen.genPartIdx.compute()[ev]) 
+events = NanoEventsFactory.from_root(
+    {fname: "Events"},
+    schemaclass=PFNanoAODSchema,
+    metadata={"dataset": "signal"},
+    delayed = False).events()
+
+output = processor_instance.process(events)
+
+print(output["n_leptons"])
