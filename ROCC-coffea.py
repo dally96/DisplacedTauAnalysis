8aa9063583e3d8f93dc41bca24449136adefe9be
Daniel Ally <d.ally@cern.ch>
2024-12-05 17:52:15 +0100

    Changed dR2 method to .nearest()

--- ROCC-coffea.py
+++ ROCC-coffea.py
@@ -17,7 +17,7 @@ events = NanoEventsFactory.from_root(
     {fname: "Events"},
     schemaclass=PFNanoAODSchema,
     metadata={"dataset": "signal"},
-    delayed = False).events()
+    delayed = True).events()
 
 # Selection cut parameters
 min_pT = 20
@@ -26,10 +26,20 @@ dr2 = 0.3**2
 
 # Prompt and signal selection
 #prompt_sig_mask = events[events.GenPart.hasFlags(['isPrompt', 'isLastCopy'])]
-dR2_mask = events[(
-    (events.Jet.phi - events.GenPart.phi)**2 +
-    (events.Jet.eta - events.GenPart.eta)**2)
-    < 0.4**2]
+#dR2_mask = events[(
+#    (events.Jet.phi - events.GenPart.phi)**2 +
+#    (events.Jet.eta - events.GenPart.eta)**2)
+#    < 0.4**2]
+
+dr2_mask = events.Jet.nearest(events.GenPart, threshold = 0.3) 
+print(f"Jet phi {events.Jet.phi[0].compute()}")
+print(f"Jet eta {events.Jet.eta[0].compute()}")
+print(f"GenPart phi {events.GenPart.phi[0].compute()}")
+print(f"GenPart eta {events.GenPart.eta[0].compute()}") 
+print(f"Jet score {events.Jet.disTauTag_score1[0].compute()}")
+print(dr2_mask.pdgId[0].compute())
+print(dr2_mask.genPartIdxMother[0].compute())
+print(events.GenPart.pdgId[0][dr2_mask.genPartIdxMother[0]].compute())
 
 # Lepton selection
 electrons = events.Electron
@@ -48,7 +58,7 @@ tau_selection = taus[
 
 # debug
 #print("Prompt and signal mask \n", prompt_sig_mask, "\n")
-print("dR^2 mask \n", dR2_mask, "\n")
-print("Electron selection \n", electron_selection, "\n")
-print("Muon selection \n", muon_selection, "\n")
-print("Tau selection \n ", tau_selection)
+#print("dR^2 mask \n", dR2_mask, "\n")
+#print("Electron selection \n", electron_selection, "\n")
+#print("Muon selection \n", muon_selection, "\n")
+#print("Tau selection \n ", tau_selection)
