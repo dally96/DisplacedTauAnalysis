39528a30b3af9c41e39e9c7923748799797d8fad
j-s-ashley <jashley6@vols.utk.edu>
2024-12-05 16:15:03 +0100

    Working lepton selection, :bug: prompt, sig, dR2 mask

--- ROCC-coffea.py
+++ ROCC-coffea.py
@@ -6,36 +6,49 @@ import awkward as ak
 from coffea.nanoevents import NanoEventsFactory, NanoAODSchema, PFNanoAODSchema
 from coffea.analysis_tools import PackedSelection
 
-# --- Selection Cut Parameters --- #
-min_pT = 20
-max_eta = 2.4
-
+# Silence obnoxious warning
 NanoAODSchema.warn_missing_crossrefs = False
 
+# Import dataset
 fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root"
 
+# Pass dataset info to coffea object
 events = NanoEventsFactory.from_root(
     {fname: "Events"},
     schemaclass=PFNanoAODSchema,
     metadata={"dataset": "signal"},
     delayed = False).events()
 
-gpart = events.GenPart
-electron_selection = gpart[ (abs(gpart.pdgId) == 11) &\
-        (gpart.pt > min_pT) &\
-        (gpart.eta < max_eta)&\
-        (gpart.hasFlags("isLastCopy")) ]
-muon_selection = gpart[ (abs(gpart.pdgId) == 13) &\
-        (gpart.pt > min_pT) &\
-        (gpart.eta < max_eta)&\
-        (gpart.hasFlags("isLastCopy")) ]
-tau_selection = gpart[ (abs(gpart.pdgId) == 15) &\
-        (gpart.pt > min_pT) &\
-        (gpart.eta < max_eta)&\
-        (gpart.hasFlags("isLastCopy")) ]
-print("ELECTRONS")
-print(electron_selection)
-print("MUONS")
-print(muon_selection)
-print("TAUS")
-print(tau_selection)
+# Selection cut parameters
+min_pT = 20
+max_eta = 2.4
+dr2 = 0.3**2
+
+# Prompt and signal selection
+#prompt_sig_mask = events[events.GenPart.hasFlags(['isPrompt', 'isLastCopy'])]
+dR2_mask = events[(
+    (events.Jet.phi - events.GenPart.phi)**2 +
+    (events.Jet.eta - events.GenPart.eta)**2)
+    < 0.4**2]
+
+# Lepton selection
+electrons = events.Electron
+muons = events.Muon
+taus = events.Tau
+
+electron_selection = electrons[
+        (numpy.abs(electrons.eta) < max_eta) &
+        (electrons.pt > min_pT)]
+muon_selection = muons[
+        (numpy.abs(muons.eta) < max_eta) &
+        (muons.pt > min_pT)]
+tau_selection = taus[
+        (numpy.abs(taus.eta) < max_eta) &
+        (taus.pt > min_pT)]
+
+# debug
+#print("Prompt and signal mask \n", prompt_sig_mask, "\n")
+print("dR^2 mask \n", dR2_mask, "\n")
+print("Electron selection \n", electron_selection, "\n")
+print("Muon selection \n", muon_selection, "\n")
+print("Tau selection \n ", tau_selection)
