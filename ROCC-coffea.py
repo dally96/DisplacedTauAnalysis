fae3298318b59e4125a394117543141966bec155
Daniel Ally <d.ally@cern.ch>
2024-12-10 16:51:45 +0100

    Deleted old dR code

--- ROCC-coffea.py
+++ ROCC-coffea.py
@@ -24,27 +24,17 @@ min_pT = 20
 max_eta = 2.4
 dr2 = 0.3**2
 
-# Prompt and signal selection
-#prompt_sig_mask = events[events.GenPart.hasFlags(['isPrompt', 'isLastCopy'])]
-#dR2_mask = events[(
-#    (events.Jet.phi - events.GenPart.phi)**2 +
-#    (events.Jet.eta - events.GenPart.eta)**2)
-#    < 0.4**2]
+staus = events.GenPart[abs(events.GenPart.pdgID) == 1000015]
+stau_taus = staus.distinctChildren
 
-dr2_mask = events.Jet.nearest(events.GenPart, threshold = 0.3) 
-print(f"Jet phi {events.Jet.phi[0].compute()}")
-print(f"Jet eta {events.Jet.eta[0].compute()}")
-print(f"GenPart phi {events.GenPart.phi[0].compute()}")
-print(f"GenPart eta {events.GenPart.eta[0].compute()}") 
-print(f"Jet score {events.Jet.disTauTag_score1[0].compute()}")
-print(dr2_mask.pdgId[0].compute())
-print(dr2_mask.genPartIdxMother[0].compute())
-print(events.GenPart.pdgId[0][dr2_mask.genPartIdxMother[0]].compute())
+
+# Prompt and signal selection
+dR2_mask = events.Jet.nearest(events.GenPart, threshold = 0.3) 
 
 # Lepton selection
 electrons = events.Electron
-muons = events.Muon
-taus = events.Tau
+muons = events.DisMuon
+jets = events.Jets
 
 electron_selection = electrons[
         (numpy.abs(electrons.eta) < max_eta) &
@@ -52,13 +42,5 @@ electron_selection = electrons[
 muon_selection = muons[
         (numpy.abs(muons.eta) < max_eta) &
         (muons.pt > min_pT)]
-tau_selection = taus[
-        (numpy.abs(taus.eta) < max_eta) &
-        (taus.pt > min_pT)]
 
 # debug
-#print("Prompt and signal mask \n", prompt_sig_mask, "\n")
-#print("dR^2 mask \n", dR2_mask, "\n")
-#print("Electron selection \n", electron_selection, "\n")
-#print("Muon selection \n", muon_selection, "\n")
-#print("Tau selection \n ", tau_selection)
