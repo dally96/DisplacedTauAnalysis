1d6d3dd5438341d28bc7946f14e4d3d863af9a16
j-s-ashley <jashley6@vols.utk.edu>
2025-01-14 17:49:39 +0100

    Play with jet slice fields

--- coffea_test.py
+++ coffea_test.py
@@ -2,15 +2,14 @@ import uproot
 import scipy
 import awkward as ak
 import numpy as np
-from coffea import processor
 from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
-from coffea.analysis_tools import PackedSelection
 import matplotlib.pyplot as plt
 import vector
 
 NanoAODSchema.warn_missing_crossrefs = False
 
-fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root"
+fname = "/eos/user/d/dally/DisplacedTauAnalysis/SMS-TStauStau_MStau-100_ctau-100mm_mLSP-1_TuneCP5_13p6TeV_NanoAOD.root" # signal
+#fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_1-1.root" # background
 
 events = NanoEventsFactory.from_root(
     {fname: "Events"},
@@ -18,11 +17,19 @@ events = NanoEventsFactory.from_root(
     metadata={"dataset": "signal"},
     delayed = False).events()
 
-selection = PackedSelection()
-tau_pt_vals = events.Tau.pt
-selection.add("tau_pt", tau_pt_vals > 2)
-selection.add("event_cut", selection.require(tau_pt=true))
-selected_events = events[selection.all("event_cut")]
+dr_max = 0.4 
 
-print(f"Tau pt values\n{tau_pt_vals}\n")
-print(f"Min tau pt after cut\n{min(selected_events.Tau.pt)}")
+taus = events.GenPart[events.GenVisTau.genPartIdxMother] # hadronically-decaying taus
+stau_taus = taus[abs(taus.distinctParent.pdgId) == 1000015] # h-decay taus with stau parents
+
+jets = events.Jet
+scores = jets.disTauTag_score1
+tau_jets = stau_taus.nearest(events.Jet, threshold = dr_max) # jets dr-matched to stau_taus
+#matched_scores = tau_jets.disTauTag_score1 # scores of matched jets
+
+true_tau_jet_mask = ak.any(jets.genJetIdxG[:, None] == tau_jets.genJetIdxG, axis=-1)
+true_tau_jets = jets[true_tau_jet_mask]
+false_tau_jets = jets[~true_tau_jet_mask]
+
+print("events.GenVisTau.genPartIdxMother")
+print(events.GenVisTau.genPartIdxMother)
