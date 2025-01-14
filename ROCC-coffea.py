443902f4e515a31031401e74929450852576af76
j-s-ashley <jashley6@vols.utk.edu>
2025-01-14 17:50:11 +0100

    Use bg file for fake rate

--- ROCC-coffea.py
+++ ROCC-coffea.py
@@ -10,14 +10,19 @@ from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 NanoAODSchema.warn_missing_crossrefs = False
 
 # Import dataset
-fname = "/eos/user/d/dally/DisplacedTauAnalysis/SMS-TStauStau_MStau-100_ctau-100mm_mLSP-1_TuneCP5_13p6TeV_NanoAOD.root" # signal
-#fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_1-1.root" # background
+signal_fname = "/eos/user/d/dally/DisplacedTauAnalysis/SMS-TStauStau_MStau-100_ctau-100mm_mLSP-1_TuneCP5_13p6TeV_NanoAOD.root" # signal
+bg_fname = "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_1-1.root" # background
 
-# Pass dataset info to coffea object
-events = NanoEventsFactory.from_root(
-    {fname: "Events"},
+# Pass dataset info to coffea objects
+signal_events = NanoEventsFactory.from_root(
+    {signal_fname: "Events"},
     schemaclass=NanoAODSchema,
     metadata={"dataset": "signal"},
+    delayed = False).events()
+bg_events = NanoEventsFactory.from_root(
+    {bg_fname: "Events"},
+    schemaclass=NanoAODSchema,
+    metadata={"dataset": "background"},
     delayed = False).events()
 
 # Selection cut parameters
@@ -26,48 +31,49 @@ min_pT = 20
 max_eta = 2.4
 max_dR2 = 0.3**2
 dr_max = 0.4
-resolution = 4 # inverse, 50 is probably a good value once functionality is confirmed
+inv_res = 4 # inverse resolution, 50 is probably a good value once functionality is confirmed
 
-taus = events.GenPart[events.GenVisTau.genPartIdxMother] # hadronically-decaying taus
+# Signal processing
+taus = signal_events.GenPart[signal_events.GenVisTau.genPartIdxMother] # hadronically-decaying taus
 stau_taus = taus[abs(taus.distinctParent.pdgId) == 1000015] # h-decay taus with stau parents
+signal_jets = signal_events.Jet
+true_tau_jets = stau_taus.nearest(signal_events.Jet, threshold = dr_max) # jets dr-matched to stau_taus
+matched_signal_scores = true_tau_jets.disTauTag_score1
 
-jets = events.Jet
-scores = jets.disTauTag_score1
-tau_jets = stau_taus.nearest(events.Jet, threshold = dr_max) # jets dr-matched to stau_taus
-matched_scores = tau_jets.disTauTag_score1 # scores of matched jets
+# Background processing
+bg_jets = bg_events.Jet
+bg_scores = bg_jets.disTauTag_score1
+false_tau_jets = bg_jets # No staus present in bg
+matched_bg_scores = bg_scores
 
-true_tau_jet_mask = ak.any(jets.genJetIdxG[:, None] == tau_jets.genJetIdxG, axis=-1)
-true_tau_jets = jets[true_tau_jet_mask]
-false_tau_jets = jets[~true_tau_jet_mask]
+# Jet totals
+total_true_tau_jets = ak.sum(ak.num(true_tau_jets))
+total_false_tau_jets = ak.sum(ak.num(false_tau_jets))
 
+total_jets = (
+    ak.sum(ak.num(signal_jets)) +
+    ak.sum(ak.num(bg_jets)) )
+
+# ROC calculations
 fake_rates = []
 efficiencies = []
 
-for score_threshold in range(0, resolution+1):
-    threshold = score_threshold / resolution
-    passing_scores_mask = matched_scores >= threshold
-    passing_scores = matched_scores[passing_scores_mask]
+for increment in range(0, inv_res+1):
+    threshold = increment / inv_res
 
-    passing_jet_mask = scores >= threshold
-    passing_jets = jets[passing_jet_mask]
+    passing_signal_mask = matched_signal_scores >= threshold
+    true_passing_jets = true_tau_jets[passing_signal_mask]
 
-    true_passing_mask = ak.any(passing_jets.genJetIdxG[:, None] == true_tau_jets.genJetIdxG, axis=-1)
-    true_passing_jets = jets[true_passing_mask]
-
-    false_passing_mask = ak.any(passing_jets.genJetIdxG[:, None] == false_tau_jets.genJetIdxG, axis=-1)
-    false_passing_jets = jets[false_passing_mask]
+    passing_bg_mask = matched_bg_scores >= threshold
+    false_passing_jets = false_tau_jets[passing_bg_mask]
 
     # --- Totals --- #
-    total_passing_scores = ak.sum(ak.num(matched_scores))
-    total_scores = ak.sum(ak.num(scores))
-    total_true_tau_jets = ak.sum(ak.num(true_tau_jets))
     total_true_passing_jets = ak.sum(ak.num(true_passing_jets))
-    total_false_tau_jets = ak.sum(ak.num(false_tau_jets))
     total_false_passing_jets = ak.sum(ak.num(false_passing_jets))
 
     # --- Results --- #
     efficiency = total_true_passing_jets / total_true_tau_jets
-    fake_rate = total_false_passing_jets / total_scores
+    fake_rate = total_false_passing_jets / total_jets
 
     efficiencies.append(efficiency)
     fake_rates.append(fake_rate)
@@ -77,9 +83,3 @@ for score_threshold in range(0, resolution+1):
 
 print("List of efficiencies", efficiencies)
 print("List of fake rates", fake_rates)
-print(ak.sum(ak.num(tau_jets)), " tau_jet entries, compared to")
-print(total_true_tau_jets, "true tau jet entries")
-print("tau_jets")
-print(tau_jets.genJetIdxG)
-print("true_tau_jets")
-print(true_tau_jets.genJetIdxG)
