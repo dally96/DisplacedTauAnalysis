281a5becf05430b52632a99a0de2f3277ff3fdbe
j-s-ashley <jashley6@vols.utk.edu>
2025-02-18 14:58:28 +0100

    Test fileset functionality

---
+++ big-bg-test.py
@@ -0,0 +1,138 @@
+import coffea
+import uproot
+import scipy
+import dask
+import numpy as np
+import awkward as ak
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from coffea import processor
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+from coffea.dataset_tools import (
+    apply_to_fileset,
+    max_chunks,
+    preprocess,
+)
+
+# Prevent lepton veto problems
+NanoAODSchema.mixins["DisMuon"] = "Muon"
+
+# Silence obnoxious warning
+NanoAODSchema.warn_missing_crossrefs = False
+
+# Selection cut parameters
+min_pT = 20
+max_eta = 2.4
+max_dr = 0.3
+max_lep_dr = 0.4
+score_increment_scale_factor = 500
+
+def delta_r_mask(first: ak.highlevel.Array, second: ak.highlevel.Array, threshold: float) -> ak.highlevel.Array:
+            mval = first.metric_table(second)
+            return ak.all(mval > threshold, axis=-1)
+
+def apply_cuts(collection):
+    pt_mask = collection.pt >= min_pT
+    eta_mask = abs(collection.eta) < max_eta
+    valid_mask = collection.genJetIdx > 0
+    
+    collection_length = ak.sum(ak.num(collection.partonFlavour))
+    inclusion_mask = collection.genJetIdx < collection_length
+    
+    cut_collection = collection[
+        pt_mask & eta_mask & valid_mask & inclusion_mask ]
+    
+    return cut_collection
+
+def apply_lepton_veto(evt_collection: ak.highlevel.Array):
+    evt_collection['Jet'] = evt_collection.Jet[
+        delta_r_mask(evt_collection.Jet, evt_collection.Photon, max_lep_dr) ]
+    evt_collection['Jet'] = evt_collection.Jet[
+        delta_r_mask(evt_collection.Jet, evt_collection.Electron, max_lep_dr) ]
+    evt_collection['Jet'] = evt_collection.Jet[
+        delta_r_mask(evt_collection.Jet, evt_collection.DisMuon, max_lep_dr) ]
+    return evt_collection
+
+class BGProcessor(processor.ProcessorABC):
+    def __init__(self):
+        pass
+
+    def process(self,events):
+        dataset = events.metadata['dataset']
+        jets = ak.zip(
+            {
+                "pt": events.Jet.pt,
+                "eta": events.Jet.eta,
+                "genJetIdx": events.Jet.genJetIdx,
+                "partonFlavour": events.Jet.partonFlavour,
+                "disTauTag_score1": events.Jet.disTauTag_score1,
+            },
+        )
+        
+        cut = apply_cuts(jets)
+        
+        return {
+            dataset: {
+                "jets": cut,
+            }
+        }
+    def postprocess(self,accumulator):
+        pass
+
+# Import dataset
+fileset = {
+    'TT to 4Q' : {
+        "files": {
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_1-1.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_1-2.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_1-3.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_1.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_10.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_11.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_12.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_13.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_14.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_15.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_16.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_17.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_18.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_2.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_20.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_21.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_22.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_23.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_24.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_25.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_26.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_27.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_28.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_29.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_30.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_3.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_4.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_5.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_6.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_7.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_8.root': "Events",
+            '/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215611/0000/nanoaod_output_9.root': "Events",
+            }
+    }
+}
+
+dataset_runnable, dataset_updated = preprocess(
+    fileset,
+    align_clusters=False,
+    step_size=100_000,
+    files_per_batch=1,
+    skip_bad_files=False,
+    save_form=False,
+)
+
+to_compute = apply_to_fileset(
+    BGProcessor(),
+    max_chunks(dataset_runnable, 300),
+    schemaclass=NanoAODSchema,
+)
+
+(cut_bg_jets,) = dask.compute(to_compute)
+print(cut_bg_jets)
