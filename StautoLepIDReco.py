af521732b13d6bea4ee26a108e5f56462a05f7bf
dally96 <dally@vols.utk.edu>
2025-05-29 15:25:20 -0500

    Storing changes to  StautoLepIDReco

--- StautoLepIDReco.py
+++ StautoLepIDReco.py
@@ -4,15 +4,30 @@ import matplotlib as mpl
 import awkward as ak
 import dask_awkward as dak
 import numpy as np
-import math
-import ROOT
 import array
-import pandas as pd
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-from leptonPlot import *
-from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+import ROOT
 
+import dask
+from dask import config as cfg
+cfg.set({'distributed.scheduler.worker-ttl': None}) # Check if this solves some dask issues
+from dask.distributed import Client, wait, progress, LocalCluster 
+from dask_jobqueue import HTCondorCluster
+
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+from coffea import processor
+from coffea.dataset_tools import (
+    apply_to_fileset,
+    max_chunks,
+    preprocess,
+)
+
+import warnings
+warnings.filterwarnings("ignore", module="coffea") # Suppress annoying deprecation warnings for coffea vector, c.f. https://github.com/CoffeaTeam/coffea/blob/master/src/coffea/nanoevents/methods/candidate.py
+import logging
+
+from leptonPlot import *
 
 NanoAODSchema.warn_missing_crossrefs = False
 
@@ -34,82 +49,171 @@ lxyRANGE = ["0-0.5", "0.5-1", "1-1.5", "1.5-2", "2-2.5","2.5-3", "3-3.5", "3.5-4
             "5-5.5", "5.5-6", "6-6.5", "6.5-7", "7-7.5","7.5-8", "8-8.5", "8.5-9", "9-9.5", "9.5-10",
             "10-10.5", "10.5-11", "11-11.5", "11.5-12", "12-12.5","12.5-13", "13-13.5", "13.5-14", "14-14.5", "14.5-15"]
 
+SAMP = [
+      ['Stau_100_1000mm', 'SIG'],
+      ['Stau_100_100mm', 'SIG'],
+      ['Stau_100_10mm', 'SIG'],
+      ['Stau_100_1mm', 'SIG'],
+      ['Stau_100_0p1mm', 'SIG'],
+      ['Stau_100_0p01mm', 'SIG'],
+      ['Stau_200_1000mm', 'SIG'],
+      ['Stau_200_100mm', 'SIG'],
+      ['Stau_200_10mm', 'SIG'],
+      ['Stau_200_1mm', 'SIG'],
+      ['Stau_200_0p1mm', 'SIG'],
+      ['Stau_200_0p01mm', 'SIG'],
+      ['Stau_300_1000mm', 'SIG'],
+      ['Stau_300_100mm', 'SIG'],
+      ['Stau_300_10mm', 'SIG'],
+      ['Stau_300_1mm', 'SIG'],
+      ['Stau_300_0p1mm', 'SIG'],
+      ['Stau_300_0p01mm', 'SIG'],
+      ['Stau_500_1000mm', 'SIG'],
+      ['Stau_500_100mm', 'SIG'],
+      ['Stau_500_10mm', 'SIG'],
+      ['Stau_500_1mm', 'SIG'],
+      ['Stau_500_0p1mm', 'SIG'],
+      ['Stau_500_0p01mm', 'SIG'],
+      ['QCD50_80', 'QCD'],
+      ['QCD80_120','QCD'],
+      ['QCD120_170','QCD'],
+      ['QCD170_300','QCD'],
+      ['QCD300_470','QCD'],
+      ['QCD470_600','QCD'],
+      ['QCD600_800','QCD'],
+      ['QCD800_1000','QCD'],
+      ['QCD1000_1400','QCD'],
+      ['QCD1400_1800','QCD'],
+      ['QCD1800_2400','QCD'],
+      ['QCD2400_3200','QCD'],
+      ['QCD3200','QCD'],
+      ["DYJetsToLL", 'EWK'],  
+      #["WtoLNu2Jets", 'EWK'],
+      ["TTtoLNu2Q",  'TT'],
+      ["TTto4Q", 'TT'],
+      ["TTto2L2Nu", 'TT'],
+      ]
 
 ROOT.gStyle.SetOptStat(0)
 
 #file = "Staus_M_100_100mm_13p6TeV_Run3Summer22_DisMuon_GenPartMatch.root" 
-file = "SMS-TStauStau_MStau-100_ctau-100mm_mLSP-1_TuneCP5_13p6TeV_NanoAOD.root"
+#file = "SMS-TStauStau_MStau-100_ctau-100mm_mLSP-1_TuneCP5_13p6TeV_NanoAOD.root"
  
-events = NanoEventsFactory.from_root({file:"Events"}, schemaclass=NanoAODSchema).events()
+#events = NanoEventsFactory.from_root({file:"Events"}, schemaclass=NanoAODSchema).events()
 NanoAODSchema.mixins["DisMuon"] = "Muon"
-gpart = events.GenPart
-dxy = (events.GenPart.vertexY - events.GenVtx.y) * np.cos(events.GenPart.phi) - \
-      (events.GenPart.vertexX - events.GenVtx.x) * np.sin(events.GenPart.phi)
-lxy = np.sqrt( (events.GenPart.vertexX - events.GenVtx.x) ** 2 + (events.GenPart.vertexY - events.GenVtx.y) ** 2)
-events['GenPart'] = ak.with_field(events.GenPart, dxy, where="dxy")
-events['GenPart'] = ak.with_field(events.GenPart, lxy, where="lxy")
-print(gpart.fields)
-staus = gpart[(abs(gpart.pdgId) == 1000015) & (gpart.hasFlags("isLastCopy"))]
-print(staus.fields)
-staus_taus = staus.distinctChildren[(abs(staus.distinctChildren.pdgId) == 15) & (staus.distinctChildren.hasFlags("isLastCopy"))]
-print(staus_taus.fields)
-staus_taus = ak.firsts(staus_taus[ak.argsort(staus_taus.pt, ascending=False)], axis = 2)
-gen_mu = staus_taus.distinctChildren[(abs(staus_taus.distinctChildren.pdgId) == 13) & (staus_taus.distinctChildren.hasFlags("isLastCopy"))]
+class IDProcessor(processor.ProcessorABC):
+    def __init__(self):
+        self._accumulator = {}
+        for sample in SAMP:
+            self._accumulator[sample[0]] = {}        
+            self._accumulator[sample[0]]["tight"] = {}
+            self._accumulator[sample[0]]["medium"] = {}
+            self._accumulator[sample[0]]["gen"] = {}
+ 
+    def process(self, events)    
+        id_dict = {}
+            
+        id_dict[events.metadata['dataset']] = {}
+        id_dict[events.metadata['dataset']]["tight"] = {}
+        id_dict[events.metadata['dataset']]["medium"] = {}
+        id_dict[events.metadata['dataset']]["gen"] = {}
 
-gen_mu = gen_mu[(gen_mu.pt > GenPtMin) & (abs(gen_mu.eta) < GenEtaMax)]
+        gpart = events.GenPart
+        #dxy = (events.GenPart.vertexY - events.GenVtx.y) * np.cos(events.GenPart.phi) - \
+        #      (events.GenPart.vertexX - events.GenVtx.x) * np.sin(events.GenPart.phi)
+        #lxy = np.sqrt( (events.GenPart.vertexX - events.GenVtx.x) ** 2 + (events.GenPart.vertexY - events.GenVtx.y) ** 2)
+        #events['GenPart'] = ak.with_field(events.GenPart, dxy, where="dxy")
+        #events['GenPart'] = ak.with_field(events.GenPart, lxy, where="lxy")
+        #print(gpart.fields)
+        staus = gpart[(abs(gpart.pdgId) == 1000015) & (gpart.hasFlags("isLastCopy"))]
+        #print(staus.fields)
+        staus_taus = staus.distinctChildren[(abs(staus.distinctChildren.pdgId) == 15) & (staus.distinctChildren.hasFlags("isLastCopy")) & (staus.distinctChildren.hasFlags("fromHardProcess"))]
+        #print(staus_taus.fields)
+        staus_taus = ak.firsts(staus_taus[ak.argsort(staus_taus.pt, ascending=False)], axis = 2)
+        gen_mu = staus_taus.distinctChildren[(abs(staus_taus.distinctChildren.pdgId) == 13) & (staus_taus.distinctChildren.hasFlags("isLastCopy"))]
+        
+        gen_mu = gen_mu[(gen_mu.pt > GenPtMin) & (abs(gen_mu.eta) < GenEtaMax)]
+        
+        ### Make sure that the reco muons can be traced back to a gen particle 
+        dis_mu = events.DisMuon[(events.DisMuon.genPartIdx > 0)]
+        
+        ### Make sure the sample of reco muons we're looking at have a gen particle that is the grandchild of a stau
+        reco_mu = dis_mu[(abs(events.GenPart[dis_mu.genPartIdx].distinctParent.distinctParent.pdgId) == 1000015)]
 
-dis_mu = events.DisMuon[(events.DisMuon.genPartIdx > 0)]
+        ### Separate the reco muons into different IDs
+        loosereco_mu  = reco_mu[reco_mu.looseId == 1]
+        mediumreco_mu = reco_mu[reco_mu.mediumId == 1]
+        tightreco_mu  = reco_mu[reco_mu.tightId == 1]
+        
+        ### Now choose the gen particles those reco muons trace back to
+        rfg_mu       = events.GenPart[reco_mu.genPartIdx]
 
-reco_mu = dis_mu[(abs(events.GenPart[dis_mu.genPartIdx].distinctParent.distinctParent.pdgId) == 1000015)]
-loosereco_mu  = reco_mu[reco_mu.looseId == 1]
-mediumreco_mu = reco_mu[reco_mu.mediumId == 1]
-tightreco_mu  = reco_mu[reco_mu.tightId == 1]
+        ### Choose the gen muons based on the reco muon ID
+        looserfg_mu  = events.GenPart[loosereco_mu.genPartIdx] 
+        mediumrfg_mu = events.GenPart[mediumreco_mu.genPartIdx] 
+        tightrfg_mu  = events.GenPart[tightreco_mu.genPartIdx] 
+        
+        ### Apply fiducial cuts
+        rfg_mu = rfg_mu[(rfg_mu.pt > GenPtMin) & (abs(rfg_mu.eta) < GenEtaMax)]
+        looserfg_mu = looserfg_mu[(looserfg_mu.pt > GenPtMin) & (abs(looserfg_mu.eta) < GenEtaMax)]
+        mediumrfg_mu = mediumrfg_mu[(mediumrfg_mu.pt > GenPtMin) & (abs(mediumrfg_mu.eta) < GenEtaMax)]
+        tightrfg_mu = tightrfg_mu[(tightrfg_mu.pt > GenPtMin) & (abs(tightrfg_mu.eta) < GenEtaMax)]
+        
+        
+        id_dict[events.metadata['dataset']]["gen"]["pt"]  = gen_mu.pt
+        id_dict[events.metadata['dataset']]["gen"]["eta"] = gen_mu.eta
+        #GenMu_dxy = gen_mu.dxy
+        #GenMu_lxy = gen_mu.lxy
+        
+        #RecoMuonsFromGen_pt  = rfg_mu.pt
+        #RecoMuonsFromGen_eta = rfg_mu.eta
+        #RecoMuonsFromGen_dxy = rfg_mu.dxy
+        #RecoMuonsFromGen_lxy = rfg_mu.lxy
+        
+        LooseRecoMuonsFromGen_pt   = looserfg_mu.pt
+        LooseRecoMuonsFromGen_eta  = looserfg_mu.eta
+        #LooseRecoMuonsFromGen_dxy  = looserfg_mu.dxy
+        #LooseRecoMuonsFromGen_lxy  = looserfg_mu.lxy
+        
+        id_dict[events.metadata['dataset']]["medium"]["pt"]  = mediumrfg_mu.pt
+        id_dict[events.metadata['dataset']]["medium"]["eta"]  = mediumrfg_mu.eta
+        #MediumRecoMuonsFromGen_dxy = mediumrfg_mu.dxy
+        #MediumRecoMuonsFromGen_lxy = mediumrfg_mu.lxy
+        
+        id_dict[events.metadata['dataset']]["tight"]["pt"]   = tightrfg_mu.pt
+        id_dict[events.metadata['dataset']]["tight"]["eta"]  = tightrfg_mu.eta
+        #TightRecoMuonsFromGen_dxy  = tightrfg_mu.dxy
+        #TightRecoMuonsFromGen_lxy  = tightrfg_mu.lxy
+        
+        return id_dict 
+
+    def postprocess(self):
+        pass
 
 
-rfg_mu       = events.GenPart[reco_mu.genPartIdx]
-looserfg_mu  = events.GenPart[loosereco_mu.genPartIdx] 
-mediumrfg_mu = events.GenPart[mediumreco_mu.genPartIdx] 
-tightrfg_mu  = events.GenPart[tightreco_mu.genPartIdx] 
+background_samples = {} 
+background_samples["QCD"] = []
+background_samples["TT"] = []
+background_samples["W"] = []
+background_samples["DY"] = []
 
-rfg_mu = rfg_mu[(rfg_mu.pt > GenPtMin) & (abs(rfg_mu.eta) < GenEtaMax)]
-looserfg_mu = looserfg_mu[(looserfg_mu.pt > GenPtMin) & (abs(looserfg_mu.eta) < GenEtaMax)]
-mediumrfg_mu = mediumrfg_mu[(mediumrfg_mu.pt > GenPtMin) & (abs(mediumrfg_mu.eta) < GenEtaMax)]
-tightrfg_mu = tightrfg_mu[(tightrfg_mu.pt > GenPtMin) & (abs(tightrfg_mu.eta) < GenEtaMax)]
-
-
-GenMu_pt  = gen_mu.pt
-GenMu_eta = gen_mu.eta
-GenMu_dxy = gen_mu.dxy
-GenMu_lxy = gen_mu.lxy
-
-RecoMuonsFromGen_pt  = rfg_mu.pt
-RecoMuonsFromGen_eta = rfg_mu.eta
-RecoMuonsFromGen_dxy = rfg_mu.dxy
-RecoMuonsFromGen_lxy = rfg_mu.lxy
-
-LooseRecoMuonsFromGen_pt   = looserfg_mu.pt
-LooseRecoMuonsFromGen_eta  = looserfg_mu.eta
-LooseRecoMuonsFromGen_dxy  = looserfg_mu.dxy
-LooseRecoMuonsFromGen_lxy  = looserfg_mu.lxy
-
-MediumRecoMuonsFromGen_pt  = mediumrfg_mu.pt
-MediumRecoMuonsFromGen_eta = mediumrfg_mu.eta
-MediumRecoMuonsFromGen_dxy = mediumrfg_mu.dxy
-MediumRecoMuonsFromGen_lxy = mediumrfg_mu.lxy
-
-TightRecoMuonsFromGen_pt   = tightrfg_mu.pt
-TightRecoMuonsFromGen_eta  = tightrfg_mu.eta
-TightRecoMuonsFromGen_dxy  = tightrfg_mu.dxy
-TightRecoMuonsFromGen_lxy  = tightrfg_mu.lxy
-
-print(f"GenMuon pt looks like {ak.flatten(GenMu_pt, axis = None).compute()}")
-print(f"The number of gen muon is {len(ak.flatten(GenMu_pt, axis = None).compute())}")
+for samples in SAMP:
+    if "QCD" in samples[0]:
+        background_samples["QCD"].append( ("/eos/uscms/store/user/dally/second_skim_muon_root/merged/merged_SRcuts_noID_noJetDxy" + samples[0] + "/*.root", xsecs[samples[0]] * lumi * 1000 * 1/num_events[samples[0]]))
+    if "TT" in samples[0]:
+        background_samples["TT"].append(  ("/eos/uscms/store/user/dally/second_skim_muon_root/merged/merged_SRcuts_noID_noJetDxy" + samples[0] + "/*.root", xsecs[samples[0]] * lumi * 1000 * 1/num_events[samples[0]]))
+    if "W" in samples[0]:
+        background_samples["W"].append(   ("/eos/uscms/store/user/dally/second_skim_muon_root/merged/merged_SRcuts_noID_noJetDxy" + samples[0] + "/*.root", xsecs[samples[0]] * lumi * 1000 * 1/num_events[samples[0]]))
+    if "DY" in samples[0]:
+        background_samples["DY"].append(  ("/eos/uscms/store/user/dally/second_skim_muon_root/merged/merged_SRcuts_noID_noJetDxy" + samples[0] + "/*.root", xsecs[samples[0]] * lumi * 1000 * 1/num_events[samples[0]]))
+    if "Stau" in samples[0]:
+        background_samples[samples[0]] = [("/eos/uscms/store/user/dally/second_skim_muon_root/merged/merged_SRcuts_noID_noJetDxy" + samples[0] + "/*.root", xsecs[samples[0]] * lumi * 1000 * 1/num_events[samples[0]])]
 
 ### Efficiency Plots
-makeEffPlot("mu", "ID", ["No ID", "Tight", "Med", "Loose"], "pt", 16, 20, 100, 5, "[GeV]",  [GenMu_pt.compute(),]*4,   [RecoMuonsFromGen_pt.compute(),  TightRecoMuonsFromGen_pt.compute(),  MediumRecoMuonsFromGen_pt.compute(),  LooseRecoMuonsFromGen_pt.compute()], 0, file)
-makeEffPlot("mu", "ID", ["No ID", "Tight", "Med", "Loose"], "eta", 24, -2.4, 2.4, 0.2, " ", [GenMu_eta.compute(),]*4, [RecoMuonsFromGen_eta.compute(), TightRecoMuonsFromGen_eta.compute(), MediumRecoMuonsFromGen_eta.compute(), LooseRecoMuonsFromGen_eta.compute()], 0, file)
-makeEffPlot("mu", "ID", ["No ID", "Tight", "Med", "Loose"], "dxy", 30, 0, 15, 0.5, "[cm]",  [GenMu_dxy.compute(),]*4,  [RecoMuonsFromGen_dxy.compute(), TightRecoMuonsFromGen_dxy.compute(), MediumRecoMuonsFromGen_dxy.compute(), LooseRecoMuonsFromGen_dxy.compute()], 0, file)
-makeEffPlot("mu", "ID", ["No ID", "Tight", "Med", "Loose"], "lxy", 30, 0, 15, 0.5, "[cm]",  [GenMu_lxy.compute(),]*4,  [RecoMuonsFromGen_lxy.compute(), TightRecoMuonsFromGen_lxy.compute(), MediumRecoMuonsFromGen_lxy.compute(), LooseRecoMuonsFromGen_lxy.compute()], 0, file) 
+#makeEffPlot("mu", "ID", ["No ID", "Tight", "Med", "Loose"], "pt", 16, 20, 100, 5, "[GeV]",  [GenMu_pt.compute(),]*4,   [RecoMuonsFromGen_pt.compute(),  TightRecoMuonsFromGen_pt.compute(),  MediumRecoMuonsFromGen_pt.compute(),  LooseRecoMuonsFromGen_pt.compute()], 0, file)
+#makeEffPlot("mu", "ID", ["No ID", "Tight", "Med", "Loose"], "eta", 24, -2.4, 2.4, 0.2, " ", [GenMu_eta.compute(),]*4, [RecoMuonsFromGen_eta.compute(), TightRecoMuonsFromGen_eta.compute(), MediumRecoMuonsFromGen_eta.compute(), LooseRecoMuonsFromGen_eta.compute()], 0, file)
+#makeEffPlot("mu", "ID", ["No ID", "Tight", "Med", "Loose"], "dxy", 30, 0, 15, 0.5, "[cm]",  [GenMu_dxy.compute(),]*4,  [RecoMuonsFromGen_dxy.compute(), TightRecoMuonsFromGen_dxy.compute(), MediumRecoMuonsFromGen_dxy.compute(), LooseRecoMuonsFromGen_dxy.compute()], 0, file)
+#makeEffPlot("mu", "ID", ["No ID", "Tight", "Med", "Loose"], "lxy", 30, 0, 15, 0.5, "[cm]",  [GenMu_lxy.compute(),]*4,  [RecoMuonsFromGen_lxy.compute(), TightRecoMuonsFromGen_lxy.compute(), MediumRecoMuonsFromGen_lxy.compute(), LooseRecoMuonsFromGen_lxy.compute()], 0, file) 
 #
 #makeEffPlot("e", "ID", ["No ID", "Tight", "Med", "Loose"], "pt", 16, 20, 100, 5, "[GeV]", GenE_pt, [RecoElectronsFromGen_pt, TightRecoElectronsFromGen_pt, MediumRecoElectronsFromGen_pt, LooseRecoElectronsFromGen_pt], 0, file)
 #makeEffPlot("e", "ID", ["No ID", "Tight", "Med", "Loose"], "eta", 24, -2.4, 2.4, 0.2, " ", GenE_eta, [RecoElectronsFromGen_eta, TightRecoElectronsFromGen_eta, MediumRecoElectronsFromGen_eta, LooseRecoElectronsFromGen_eta], 0, file)
