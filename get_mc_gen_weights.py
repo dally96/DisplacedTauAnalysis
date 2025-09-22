542c7f43212a1bfa3f7575376f346c880f97488a
sara <sara.fiorendi@cern.ch>
2025-09-22 17:33:13 +0200

    speedup, all samples, consistent capitalisation

--- get_mc_gen_weights.py
+++ get_mc_gen_weights.py
@@ -1,35 +1,35 @@
 import uproot, importlib
 import awkward as ak
-import pdb
-import json, argparse
+import json
+import argparse
+from concurrent.futures import ThreadPoolExecutor
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument(
-	"--sample",
-	choices=['QCD','DY', 'signal'],
-	required=True,
-	help='Specify the sample you want to process')
+    "--sample",
+    choices=['QCD', 'DY', 'signal', 'WtoLNu', 'Wto2Q', 'TT'],
+    required=True,
+    help="Specify the sample you want to process")
 parser.add_argument(
-	"--subsample",
-	nargs='*',
-	default='all',
-	required=False,
-	help='Specify the exact sample you want to process')
+    "--subsample",
+    nargs="*",
+    default="all",
+    required=False,
+    help="Specify the exact sample you want to process")
 parser.add_argument(
-	"--nfiles",
-	default='-1',
-	required=False,
-	help='Specify the number of input files to process')
+    "--nfiles",
+    type=int,
+    default=-1,
+    required=False,
+    help="Specify the number of input files to process")
 args = parser.parse_args()
 
-import json
 def remap_keys(mapping):
-    return [{'lumisections':k, 'sumgenw': v[0], 'ngen': v[1]} for k, v in mapping.items()]
-
+    return [{'lumisections': k, 'sumgenw': v[0], 'ngen': v[1]} for k, v in mapping.items()]
 
 samples = {
-    "Wto2Q": "samples.fileset_WTo2Q",
-    "WtoLNu": "samples.fileset_WToLNu",
+    "Wto2Q": "samples.fileset_Wto2Q",
+    "WtoLNu": "samples.fileset_WtoLNu",
     "QCD": "samples.fileset_QCD",
     "DY": "samples.fileset_DY",
     "signal": "samples.fileset_signal",
@@ -40,26 +40,44 @@ the_sample = args.sample
 module = importlib.import_module(samples[the_sample])
 input_dataset = module.fileset  
 
-if args.subsample == 'all':
+if args.subsample == "all":
     fileset = input_dataset
-else:  
+else:
     fileset = {k: input_dataset[k] for k in args.subsample}
-  
-ls_weight_dict = {}
-for idataset in fileset.keys():
-    files = fileset[idataset]['files']
-    print ('n files:', len(files))
-    for i,ifile in enumerate(files): 
-#         if i > 2:  break   
-        if (i % 10 == 0):  print ('processing file', i)
-        file = uproot.open(ifile)
-        lumiTree = file['LuminosityBlocks']
-        lumis = lumiTree["luminosityBlock"].array().to_list()
-    
-        genTree = file['Runs']
-        sumGenW = genTree['genEventSumw'].array().to_list()[0]
-        sumGenN = genTree['genEventCount'].array().to_list()[0]
-        ls_weight_dict[tuple(lumis)] = [sumGenW,sumGenN]
 
-    with open(f'samples/ls_sumw_dict_{the_sample}_{idataset}.json', 'w') as fp:  
-        json.dump(remap_keys(ls_weight_dict), fp) 
+def process_file(ifile):
+    """Read luminosity + gen info from a single ROOT file"""
+    try:
+        with uproot.open(ifile) as file:
+            lumis = file["LuminosityBlocks/luminosityBlock"].array(library="np")
+            runs = file["Runs"]
+            sumGenW = runs["genEventSumw"].array(library="np")[0]
+            sumGenN = runs["genEventCount"].array(library="np")[0]
+        # Store lumis as a list (JSON-friendly)
+        return {"lumisections": lumis.tolist(),
+                "sumgenw": float(sumGenW),
+                "ngen": int(sumGenN)}
+    except Exception as e:
+        print(f"Error processing {ifile}: {e}")
+        return None
+
+for idataset, dataset_info in fileset.items():
+    files = dataset_info["files"]
+    if args.nfiles > 0:
+        files = files[:args.nfiles]
+    print(f"Dataset {idataset}, n files: {len(files)}")
+
+    results = []
+    # ThreadPoolExecutor is usually better for uproot (I/O bound)
+    with ThreadPoolExecutor() as executor:
+        for idx, result in enumerate(executor.map(process_file, files)):
+            if idx % 100 == 0:
+                print("Processing file", idx)
+            if result:
+                results.append(result)
+
+    out_path = f"samples/ls_sumw_dict_{the_sample}_{idataset}.json"
+    with open(out_path, "w") as fp:
+        json.dump(results, fp)
+
+    print(f"{out_path} written")
